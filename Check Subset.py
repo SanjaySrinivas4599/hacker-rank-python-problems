fc77f0d6e0313687af9025e2d8cac7959621f1d6
SanjaySrinivas4599 <sanjaysrinivas333@gmail.com>
2026-03-01 21:30:12 +0530

    Create Check Subset.py

---
+++ Check Subset.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+test_cases = int(input())
+
+for i in range(test_cases):
+    count_A = int(input())
+    A = list(map(int, input().split()))
+    count_B = int(input())
+    B = list(map(int, input().split()))
+    count = len(set(A).intersection(set(B)))
+    if count == count_A:
+        print(True)
+    else:
+        print(False)
